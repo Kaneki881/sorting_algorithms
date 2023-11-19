56c0fb70cf1b6870c19079d0b2abe5373bb5ccf1
YounesFetouaki <104641370+YounesFetouaki@users.noreply.github.com>
2023-11-19 18:09:25 +0000

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ * @list: Pointer to the head of the list
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *current, *prev, *temp;
+    int value;
+
+    if (!list || !*list || !(*list)->next)
+        return;
+
+    current = (*list)->next;
+
+    while (current)
+    {
+        value = current->n;
+        temp = current->next;
+        prev = current->prev;
+
+        while (prev && value < prev->n)
+        {
+            if (prev->prev)
+                prev->prev->next = current;
+
+            current->prev = prev->prev;
+            prev->next = current->next;
+            prev->prev = current;
+
+            if (current->next)
+                current->next->prev = prev;
+
+            current->next = prev;
+            prev = current->prev;
+
+            if (!prev)
+                *list = current;
+
+            print_list(*list);
+        }
+
+        current = temp;
+        if (!current)
+            break;
+        prev = current->prev;
+    }
+}
--- sort.h
+++ sort.h
@@ -23,15 +23,15 @@ typedef struct listint_s
 
 /** Prototypes of functions **/
 void bubble_sort(int *array, size_t size);
-/* void insertion_sort_list(listint_t **list); */
+void insertion_sort_list(listint_t **list);
 void print_array(const int *array, size_t size);
-/*void selection_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void shell_sort(int *array, size_t size);
 void swap(int *a, int *b);
 size_t partition(int *array, size_t low, size_t high, size_t size);
 void quick_sort(int *array, size_t size);
 void quick_sort_recursion(int *array, size_t low, size_t high, size_t size);
-void counting_sort(int *array, size_t size); */
+void counting_sort(int *array, size_t size);
 
 #endif
