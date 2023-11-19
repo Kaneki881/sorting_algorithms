803fdb916e131d475d42e4ff8e91644b2edb03ef
YounesFetouaki <104641370+YounesFetouaki@users.noreply.github.com>
2023-11-19 18:34:25 +0000

    cocktail

--- sort.h
+++ sort.h
@@ -33,5 +33,7 @@ size_t partition(int *array, size_t low, size_t high, size_t size);
 void quick_sort(int *array, size_t size);
 void quick_sort_recursion(int *array, size_t low, size_t high, size_t size);
 void counting_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 
 #endif
