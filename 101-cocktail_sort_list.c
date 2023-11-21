a7f8d8bfe1d72df23176910e970be82073ef4ac5
YounesFetouaki <104641370+YounesFetouaki@users.noreply.github.com>
2023-11-21 08:48:21 +0000

    :)

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -29,11 +29,11 @@ void cocktail_sort_list(listint_t **list)
             break;
 
         swapped = 0;
-        for (; current->prev != NULL; current = current->prev)
+        for (current = current->prev; current != NULL; current = current->prev)
         {
-            if (current->n < current->prev->n)
+            if (current->n > current->next->n)
             {
-                swap_nodes(list, current->prev, current);
+                swap_nodes(list, current, current->next);
                 swapped = 1;
                 print_list(*list);
             }
@@ -57,6 +57,8 @@ void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 
     if (node2->next != NULL)
         node2->next->prev = node1;
+    else
+        *list = node1;
 
     node1->next = node2->next;
     node2->prev = node1->prev;
